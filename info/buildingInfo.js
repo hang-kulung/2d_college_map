63991b8cbf1b973a4504aa05ade1741c60411979
Ninamhang Kulung <hangkulung21@gmail.com>
2025-06-16 19:56:45 +0545

    xRevert "panel removed"
    This reverts commit 4b7a6f5c53013662d122c2cfd9780c197a959895.

---
+++ info/buildingInfo.js
@@ -0,0 +1,57 @@
+const buildingInfo = {
+    library: {
+        name: "Central Library",
+        description: "A vast collection of books and digital resources."
+        
+    },
+    basketball: {
+        name: "Basketball court",
+        description: "Play basketball anytime"
+       
+    },
+    canteen: {
+        name: "Canteen",
+        description: "A nice place to talk over a cup of tea"
+        
+    },
+    bhatta_park: {
+        name: "Bhatta Park",
+        description: "Best place to soak the sun during winter"
+        
+    },
+    rac: {
+        name: "RAC",
+        description: "Robotics and Automation Centre"
+        
+    },
+    a_block: {
+        name: "A Block(Workshop)",
+        description: "A lot of machines and stuff"
+        
+    },
+    b_block: {
+        name: "B Block",
+        description: "Civil Department"
+        
+    },
+    c_block: {
+        name: "C Block",
+        description: "A nice place to study"
+        
+    },
+    d_block: {
+        name: "D Block",
+        description: "A nice place to study"
+        
+    },
+    e_block: {
+        name: "ECAST Block",
+        description: "Place for Intelligent people"
+        
+    },
+    f_block: {
+        name: "F Block",
+        description: "Beautiful place"
+        
+    }
+};
